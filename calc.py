175993a2410009bfed4b2b301f2093dabb69a844
Michael Gadda <gaddami@oregonstate.edu>
2021-04-15 16:55:38 -0700

    sum of list

--- calc.py
+++ calc.py
@@ -4,6 +4,11 @@ def calc(a,b):
 	multiply = a * b
 	divide = a / b
 	list = [sum, difference, multiply, divide]
-	print(list)
+	sum1 = 0
+	for i in list:
+		sum1 +=1
+
+	sum_of_list = 
+	print(list1)
 
 calc(3,4)
