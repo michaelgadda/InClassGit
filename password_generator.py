3e4d6c12c5db602d26504fa6ad1d8f5e1b466f6e
michaelgadda <gaddami@oregonstate.edu>
2021-04-15 18:27:26 -0700

    fully functional program

--- password_generator.py
+++ password_generator.py
@@ -1,3 +1,13 @@
 #Enter the number of letters you would like to be in your password
 import random 
+import string
 def password_generator(n): 
+	temp_num = 0
+	pass_string = random.choice(string.ascii_letters)  
+	for i in range(n-1): 
+		temp_num = random.randint(1,9)
+		pass_string = str(pass_string) + str(temp_num) 
+
+	print(pass_string)
+	return pass_string 
+password_generator(10)
